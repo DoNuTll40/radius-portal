3767fd4e4fc0a9dc137c309a75c0097e3dc58572
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2025-04-25 13:52:40 +0700

    update code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,25 +17,44 @@ export default function LoginPage() {
     <div className="p-8 max-w-md mx-auto bg-white rounded shadow">
       <h1 className="text-2xl font-bold mb-4 text-center">Login Network</h1>
 
-      {/* ฟอร์มแสดงผล */}
-      <form method="POST" action={`http://192.168.106.1:1000/fgtauth`}>
-        <input type="hidden" name='magic' value={magic} />
+      {/* ฟอร์มที่ส่งข้อมูลไปยัง FortiGate */}
+      <form
+        method="POST"
+        action={`http://192.168.106.1:1000/fgtauth`}
+      >
+        {/* ส่งกลับเว็บเดิมเมื่อ login สำเร็จ (แนะนำให้ใส่) */}
+        <input
+          type="hidden"
+          name="magic"
+          value={magic}
+        />
+        <input
+          type="hidden"
+          name="4Tredir"
+          value="http://www.msftconnecttest.com/redirect"
+        />
+
+        {/* ส่ง username */}
         <input
           className="w-full p-2 border rounded mb-3"
           placeholder="Username"
-          name='username'
+          name="username"
           onChange={(e) => setUsername(e.target.value)}
           value={username}
           required
         />
+
+        {/* ส่ง password */}
         <input
           className="w-full p-2 border rounded mb-4"
           type="password"
           placeholder="Password"
+          name="password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           required
         />
+
         <button
           type="submit"
           className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
@@ -43,8 +62,8 @@ export default function LoginPage() {
           เข้าสู่ระบบ
         </button>
 
-        <div className='w-full flex justify-center'>
-          <p className='text-sm mt-5'>v 0.0.001</p>
+        <div className="w-full flex justify-center">
+          <p className="text-sm mt-5">v 0.0.002</p>
         </div>
       </form>
     </div>
