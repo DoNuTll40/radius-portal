9a489f8163c79498f085416ede08bc4424ae6e76
DoNuT_ll40 <nuttawoot.ch64@snru.ac.th>
2025-05-01 18:19:27 +0700

    update code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <div className="select-none w-full flex flex-col gap-1 justify-center items-center text-xxs text-gray-700">
-      <p className="font-bold">Captive Protal v0.9.6</p>
+      <p className="font-bold">Captive Protal v0.9.65</p>
       <p className="text-center">
         &copy; 2025 พัฒนาโดยกลุ่มงานสุขภาพดิจิทัล โรงพยาบาลอากาศอำนวย
       </p>
